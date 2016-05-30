54da4cf399fe1fdfaf6ae0d369a0bea3a02ff5ba
bhushan9sj <bhushan.jibhakate@gmail.com>
2016-05-29 20:43:49 -0700

    this is list inpython

--- lists.py
+++ lists.py
@@ -4,5 +4,6 @@ print("Second item",shopping_list[1])
 print("third item",shopping_list[2])
 print("forth item",shopping_list[3])
 print("last item",shopping_list[-1])
+
 print("second last item",shopping_list[-2])
 print("multiple items",shopping_list[1:3])
