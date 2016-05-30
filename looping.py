0c41d269361ef060d5acddff29fbf641e0fb7544
bhushan9sj <bhushan.jibhakate@gmail.com>
2016-05-30 02:48:30 -0700

    looping is use to repeating

---
+++ looping.py
@@ -0,0 +1,18 @@
+import random
+for x in range(0, 10):
+    print(x,'')
+
+print('\n')
+#new code
+shopping_list=['oats','apples','tomatoes']
+for y in shopping_list:
+    print(y)
+for x in [4,3,2,1]:
+    print(x)
+ ####new code
+
+number=[[1,2,3,4,5],[10,20,30,40,50],[100,200,300,400,500]]
+for a in range(0,5):
+    for b in range(0,5):
+        print(number[a][b])
+
---
+++ while looping.py
@@ -0,0 +1,5 @@
+import random
+random_num=random.randrange(0,500)
+while random_num !=10:
+    print(random_num)
+    random_num=random.randrange(0,500)
