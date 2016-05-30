9baf994497d25a3878eb4eef7bcaf63ac9771a42
bhushan9sj <bhushan.jibhakate@gmail.com>
2016-05-30 13:24:44 -0700

    while loop is use when we don't know how many iteration it is going to take

--- while looping.py
+++ while looping.py
@@ -3,3 +3,14 @@ random_num=random.randrange(0,500)
 while random_num !=10:
     print(random_num)
     random_num=random.randrange(0,500)
+# this will print all even numbers between 0 to 50
+i=0
+while (i <= 50):
+    if (i%2 == 0):
+        print(i)
+    elif(i == 50):
+        break
+    else:
+        i +=1
+        continue
+    i+=1
