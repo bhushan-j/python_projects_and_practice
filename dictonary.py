85d5bbca01fe7e825a2dd34b5b90f8cb73ec6841
bhushan9sj <bhushan.jibhakate@gmail.com>
2016-05-30 02:07:16 -0700

    values with unique keys Dictionaries Similar to list but cant join like lists dictionaries are surrounded with {}

--- dictonary.py
+++ dictonary.py
@@ -3,9 +3,10 @@ values with unique keys
 Dictionaries Similar to list but cant join like lists
 dictionaries are surrounded with {}
 '''
-age_data={'john':'18','smith':'20','sam':'21','Ray':'19'}
+age_data={'john':'18','smith':'20','sam':'21','ray':'19'}
 print(age_data['sam'])#this will print 21
 del age_data['sam']
-age_data['Mark']='23'
+age_data['mark']='23'
 print(age_data)
 print(len(age_data))
+print(age_data.get("mark"))
