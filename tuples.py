0741411dfe88f1cb72cbf99d16040ad05e15cf98
bhushan9sj <bhushan.jibhakate@gmail.com>
2016-05-29 22:01:50 -0700

    tuples are surrounded with () instead of [] and can not be modified easly as list.

---
+++ tuples.py
@@ -0,0 +1,4 @@
+my_tuple=(1,2,3,4,5,6,7,8,9)#tuples are surrounded with () instead of [] and can not be modified easyly as list.
+# to modify first convert tuple into list and modify and again convert list into tuple
+print (len(my_tuple),'\n',min(my_tuple),'\n',max(my_tuple))
+
