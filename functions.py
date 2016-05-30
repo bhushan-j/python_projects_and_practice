5c72870bb97210f9804da8c112c9ba81c5a8123c
bhushan9sj <bhushan.jibhakate@gmail.com>
2016-05-30 14:11:59 -0700

    while loop is use when we don't know how many iteration it is going to take

---
+++ functions.py
@@ -0,0 +1,6 @@
+def addnum(a,b):
+    add=a+b # this add variable will not work outside
+    return add
+string = addnum(10,20)
+print(string)
+print(addnum(20,50))# this will give addition of two numbers
